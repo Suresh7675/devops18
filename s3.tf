618fc6fa794574881108150aae1376a60208f3ce
Suresh7675 <softsuresh8204@gmail.com>
2026-03-08 11:01:28 +0530

    Rename S3 bucket from suresh-monolithic-77 to suresh99.flm.moonobucket

--- s3.tf
+++ s3.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "one" {
-  bucket = "terraform import aws_s3_bucket.one suresh-monolithic-77.flm"
+  bucket = "suresh99.flm.moonobucket"
 }
 
 resource "aws_s3_bucket_ownership_controls" "two" {
