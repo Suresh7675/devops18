85371b0493504582fb217b35dfdd48b52f55ad36
Suresh7675 <softsuresh8204@gmail.com>
2026-03-08 10:01:54 +0530

    Update AMI and key name in launch template

--- main.tf
+++ main.tf
@@ -1,9 +1,9 @@
 resource "aws_launch_template" "web_server_as" {
     name = "myproject"
-    image_id           = "ami-02dfbd4ff395f2a1b"
+    image_id           = "ami-0b0b78dcacbab728f"
     vpc_security_group_ids = [aws_security_group.web_server.id]
     instance_type = "t3.micro"
-    key_name = "march-kp"
+    key_name = "Monolithic-Project"
     tags = {
         Name = "DevOps"
     }
